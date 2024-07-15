412d4ba7842e044c0561e896e93d9d1371a1aa78
jotarevolver <junmcogno@gmail.com>
2024-07-15 17:43:28 -0300

    First commit innit

---
+++ src/main/java/api/hub/domain/usuario/UsuarioDTO.java
@@ -0,0 +1,14 @@
+package api.hub.domain.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UsuarioDTO(
+        @NotBlank(message = "Utilice su correo electrónico como nombre de usuario")
+        @Email(message = "Correo electrónico inválido.")
+        String email,
+
+        @NotBlank(message = "Debe tener entre 10 y 15 caracteres.")
+        String password
+) {
+}
